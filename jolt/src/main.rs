0836935e0f1a2b71444133efd93f429cc003f8c3
masatsch <kingmasatojames@gmail.com>
2025-02-02 19:10:42 +0900

    jolt supports cycles

--- jolt/src/main.rs
+++ jolt/src/main.rs
@@ -6,6 +6,8 @@ use std::{
 use jolt::host_utils::Serializable;
 use utils::benchmark;
 
+type BenchResult = (Duration, usize, usize);
+
 pub fn main() {
     // let iters = [230, 460, 920, 1840, 3680];
     // let iters = [230, 250];
@@ -40,69 +42,100 @@ pub fn main() {
     // benchmark(benchmark_bigmem, &values, "../benchmark_outputs/bigmem_jolt.csv", "value");
 }
 
-fn benchmark_sha2_chain(iters: u32) -> (Duration, usize) {
+fn benchmark_sha2_chain(iters: u32) -> BenchResult {
     let (prove_sha2_chain, _verify_sha2_chain) = sha2_chain_guest::build_sha2_chain();
     let input = [5u8; 32];
+    let program_summary = sha2_chain_guest::analyze_sha2_chain(input, iters);
 
     let start = Instant::now();
     let (_output, proof) = prove_sha2_chain(input, iters);
     let end = Instant::now();
 
-    (end.duration_since(start), proof.size().unwrap())
+    (
+        end.duration_since(start),
+        proof.size().unwrap(),
+        program_summary.processed_trace.len(),
+    )
 }
 
-fn benchmark_sha3_chain(iters: u32) -> (Duration, usize) {
+fn benchmark_sha3_chain(iters: u32) -> BenchResult {
     let (prove_sha3_chain, _verify_sha3_chain) = sha3_chain_guest::build_sha3_chain();
     let input = [5u8; 32];
+    let program_summary = sha3_chain_guest::analyze_sha3_chain(input, iters);
 
     let start = Instant::now();
     let (_output, proof) = prove_sha3_chain(input, iters);
     let end = Instant::now();
 
-    (end.duration_since(start), proof.size().unwrap())
+    (
+        end.duration_since(start),
+        proof.size().unwrap(),
+        program_summary.processed_trace.len(),
+    )
 }
 
-fn benchmark_sha2(num_bytes: usize) -> (Duration, usize) {
+fn benchmark_sha2(num_bytes: usize) -> BenchResult {
     let (prove_sha2, _verify_sha2) = sha2_guest::build_sha2();
 
     let input = vec![5u8; num_bytes];
     let input = input.as_slice();
+    let program_summary = sha2_guest::analyze_sha2(input);
 
     let start = Instant::now();
     let (_output, proof) = prove_sha2(input);
     let end = Instant::now();
 
-    (end.duration_since(start), proof.size().unwrap())
+    (
+        end.duration_since(start),
+        proof.size().unwrap(),
+        program_summary.processed_trace.len(),
+    )
 }
 
-fn benchmark_sha3(num_bytes: usize) -> (Duration, usize) {
+fn benchmark_sha3(num_bytes: usize) -> BenchResult {
     let (prove_sha3, _verify_sha3) = sha3_guest::build_sha3();
 
     let input = vec![5u8; num_bytes];
     let input = input.as_slice();
+    let program_summary = sha3_guest::analyze_sha3(input);
 
     let start = Instant::now();
     let (_output, proof) = prove_sha3(input);
     let end = Instant::now();
 
-    (end.duration_since(start), proof.size().unwrap())
+    (
+        end.duration_since(start),
+        proof.size().unwrap(),
+        program_summary.processed_trace.len(),
+    )
 }
 
-fn benchmark_fib(n: u32) -> (Duration, usize) {
+fn benchmark_fib(n: u32) -> BenchResult {
     let (prove_fib, _verify_fib) = fibonacci_guest::build_fib();
+    let program_summary = fibonacci_guest::analyze_fib(n);
 
     let start = Instant::now();
     let (_output, proof) = prove_fib(n);
     let end = Instant::now();
 
-    (end.duration_since(start), proof.size().unwrap())
+    (
+        end.duration_since(start),
+        proof.size().unwrap(),
+        program_summary.processed_trace.len(),
+    )
 }
 
-fn benchmark_bigmem(value: u32) -> (Duration, usize) {
+fn benchmark_bigmem(value: u32) -> BenchResult {
     let (prove_bigmem, verify_bigmem) = bigmem_guest::build_waste_memory();
+    let program_summary = bigmem_guest::analyze_waste_memory(value);
+
     let start = Instant::now();
     let (_output, proof) = prove_bigmem(value);
     let end = Instant::now();
 
-    (end.duration_since(start), proof.size().unwrap())
+    (
+        end.duration_since(start),
+        proof.size().unwrap(),
+        program_summary.processed_trace.len(),
+    )
 }
