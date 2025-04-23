31b7cabfa677ee0f68d8c39dba78653afa7df135
gohan <kingmasatojames@gmail.com>
2025-04-23 18:33:59 +0900

    update nexus to 3.1

--- nexus/src/fibonacci-guest/src/main.rs
+++ nexus/src/fibonacci-guest/src/main.rs
@@ -1,23 +1,9 @@
 #![cfg_attr(target_arch = "riscv32", no_std, no_main)]
 
-use nexus_rt::{read_private_input, write_output};
+use guests::fib;
 
 #[nexus_rt::main]
-fn main() {
-    let n = read_private_input::<u32>().unwrap();
-    let z = fib(n);
-    write_output::<u128>(&z)
-}
-
-fn fib(n: u32) -> u128 {
-    let mut a: u128 = 0;
-    let mut b: u128 = 1;
-    let mut sum: u128;
-    for _ in 1..n {
-        sum = a + b;
-        a = b;
-        b = sum;
-    }
-
-    b
+fn main(n: u32) -> u128 {
+    let result = fib::fib(n);
+    result
 }
