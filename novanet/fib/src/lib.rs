5570d7aaec5edbb4c3fc1c5498e8ce95ed52bf0b
gohan <kingmasatojames@gmail.com>
2025-04-23 20:07:05 +0900

    modify novanet

--- novanet/fib/src/lib.rs
+++ novanet/fib/src/lib.rs
@@ -1,13 +1,6 @@
 #![no_main]
 
 #[no_mangle]
-pub fn fib(n: u32) -> u32 {
-    let mut a = 0u32;
-    let mut b = 1u32;
-    for _ in 0..n {
-        let c = a.wrapping_add(b);
-        a = b;
-        b = c;
-    }
-    b
+pub fn fib(n: u32) -> u128 {
+    guests::fib::fib(n)
 }
