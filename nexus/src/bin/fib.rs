d0fdee5dfee42f5b0c63d1095490d0fd606a4b43
gohan <kingmasatojames@gmail.com>
2025-04-28 23:09:23 +0900

    refactor nexus

--- nexus/src/bin/fib.rs
+++ nexus/src/bin/fib.rs
@@ -4,60 +4,56 @@ use nexus_sdk::{
     stwo::seq::Stwo,
 };
 use std::time::Instant;
-use utils::{bench::BenchResult, bench::benchmark, metadata::FIBONACCI_INPUTS, size};
+use utils::{bench::Metrics, bench::benchmark_v2, metadata::FIBONACCI_INPUTS, size};
 
 const PACKAGE: &str = "fibonacci-guest";
 
 fn main() {
-    benchmark(
+    benchmark_v2(
         benchmark_fib,
         &FIBONACCI_INPUTS,
-        "../benchmark_outputs/fib_nexus.csv",
+        "../.outputs/benchmark/fib_nexus.csv",
     );
 }
 
-fn benchmark_fib(n: u32) -> BenchResult {
-    println!("Compiling guest program...");
+fn benchmark_fib(n: u32) -> Metrics {
+    let mut metrics: Metrics = Metrics::new(n as usize);
+
     let mut prover_compiler = Compiler::<CargoPackager>::new(PACKAGE);
     let prover: Stwo<Local> =
         Stwo::compile(&mut prover_compiler).expect("failed to compile guest program");
 
     let elf = prover.elf.clone(); // save elf for use with verification
 
-    println!("Proving execution of vm...");
+    let start = Instant::now();
+    let _ = prover
+        .run_with_input::<u32, ()>(&n, &())
+        .expect("failed to run program");
+    metrics.exec_duration = start.elapsed();
+    metrics.cycles = elf.instructions.len() as u64;
+
     let start = Instant::now();
     let (view, proof) = prover
         .prove_with_input::<u32, ()>(&n, &())
         .expect("failed to prove program");
-    let end = Instant::now();
-
-    println!(
-        ">>>>> Logging\n{}<<<<<",
-        view.logs().expect("failed to retrieve debug logs").join("")
-    );
-    assert_eq!(
-        view.exit_code().expect("failed to retrieve exit code"),
-        nexus_sdk::KnownExitCodes::ExitSuccess as u32
-    );
-
-    print!("Verifying execution...");
+    metrics.proof_duration = start.elapsed();
+    metrics.proof_bytes = size(&proof);
 
     let output = view
         .public_output::<u128>()
         .expect("failed to retrieve public output");
 
-    #[rustfmt::skip]
+    let start = Instant::now();
     proof
         .verify_expected(
-            &(),  // no public input
+            &(), // no public input
             nexus_sdk::KnownExitCodes::ExitSuccess as u32,
-            &output,  // no public output
-            &elf, // expected elf (program binary)
-            &[],  // no associated data,
+            &output, // no public output
+            &elf,    // expected elf (program binary)
+            &[],     // no associated data,
         )
         .expect("failed to verify proof");
+    metrics.verify_duration = start.elapsed();
 
-    println!("  Succeeded!");
-
-    (end.duration_since(start), size(&proof), 0x0)
+    metrics
 }
--- nexus/src/bin/sha2.rs
+++ nexus/src/bin/sha2.rs
@@ -4,62 +4,57 @@ use nexus_sdk::{
     stwo::seq::Stwo,
 };
 use std::time::Instant;
-use utils::{bench::BenchResult, bench::benchmark, metadata::SHA2_INPUTS, size};
+use utils::{bench::Metrics, bench::benchmark_v2, metadata::SHA2_INPUTS, sha2_input, size};
 
 const PACKAGE: &str = "sha2-guest";
 
 fn main() {
-    benchmark(
+    benchmark_v2(
         benchmark_sha2,
         &SHA2_INPUTS,
-        "../benchmark_outputs/sha2_nexus.csv",
+        "../.outputs/benchmark/sha2_nexus.csv",
     );
 }
 
-fn benchmark_sha2(num_bytes: usize) -> BenchResult {
-    let input = vec![5u8; num_bytes];
+fn benchmark_sha2(num_bytes: usize) -> Metrics {
+    let mut metrics: Metrics = Metrics::new(num_bytes as usize);
+    let input = sha2_input(num_bytes);
 
-    println!("Compiling guest program...");
     let mut prover_compiler = Compiler::<CargoPackager>::new(PACKAGE);
     let prover: Stwo<Local> =
         Stwo::compile(&mut prover_compiler).expect("failed to compile guest program");
 
     let elf = prover.elf.clone(); // save elf for use with verification
 
-    println!("Proving execution of vm...");
+    let start = Instant::now();
+    let _ = prover
+        .run_with_input::<Vec<u8>, ()>(&input, &())
+        .expect("failed to run program");
+    metrics.exec_duration = start.elapsed();
+    metrics.cycles = elf.instructions.len() as u64;
+
     let start = Instant::now();
     let (view, proof) = prover
         .prove_with_input::<Vec<u8>, ()>(&input, &())
         .expect("failed to prove program");
-    let end = Instant::now();
-
-    println!(
-        ">>>>> Logging\n{}<<<<<",
-        view.logs().expect("failed to retrieve debug logs").join("")
-    );
-    assert_eq!(
-        view.exit_code().expect("failed to retrieve exit code"),
-        nexus_sdk::KnownExitCodes::ExitSuccess as u32
-    );
-
-    print!("Verifying execution...");
+    metrics.proof_duration = start.elapsed();
+    metrics.proof_bytes = size(&proof);
 
     let output = view
         .public_output::<[u8; 32]>()
         .expect("failed to retrieve public output");
 
-    #[rustfmt::skip]
+    let start = Instant::now();
     proof
         .verify_expected(
-            &(),  // no public input
+            &(), // no public input
             nexus_sdk::KnownExitCodes::ExitSuccess as u32,
-            &output,  // no public output
-            &elf, // expected elf (program binary)
-            &[],  // no associated data,
+            &output, // no public output
+            &elf,    // expected elf (program binary)
+            &[],     // no associated data,
         )
         .expect("failed to verify proof");
+    metrics.verify_duration = start.elapsed();
 
-    println!("  Succeeded!");
-
-    (end.duration_since(start), size(&proof), 0x0)
+    metrics
 }
