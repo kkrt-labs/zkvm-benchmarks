5330924d96c9313e449c8f87b249a2110ce125a3
Oba <obatirou@gmail.com>
2025-09-08 10:49:30 +0200

    wip

--- cairo/src/main.rs
+++ cairo/src/main.rs
@@ -1,20 +1,34 @@
+use cairo_air::verifier::verify_cairo;
+use cairo_air::CairoProof;
+use cairo_air::PreProcessedTraceVariant;
+use cairo_lang_casm::hints::Hint;
+use cairo_lang_executable::executable::{EntryPointKind, Executable};
+use cairo_lang_runner::Arg;
+use cairo_lang_runner::CairoHintProcessor;
+use cairo_lang_runner::build_hints_dict;
+use cairo_vm::cairo_run::{cairo_run_program, CairoRunConfig};
+use cairo_vm::types::builtin_name::BuiltinName;
+use cairo_vm::types::layout_name::LayoutName;
+use cairo_vm::types::program::Program;
+use cairo_vm::types::relocatable::MaybeRelocatable;
+use cairo_vm::vm::runners::cairo_runner::CairoRunner;
+use cairo_vm::Felt252;
+use log::info;
+use sonic_rs;
+use std::collections::HashMap;
+use std::env;
+use stwo_cairo_adapter::builtins::MemorySegmentAddresses;
+use stwo_cairo_adapter::memory::{MemoryBuilder, MemoryConfig, MemoryEntry};
+use stwo_cairo_adapter::vm_import::{adapt_to_stwo_input, RelocatedTraceEntry};
+use stwo_cairo_adapter::{ProverInput, PublicSegmentContext};
+use stwo_cairo_prover::stwo_prover::core::fri::FriConfig;
+use stwo_cairo_prover::stwo_prover::core::pcs::PcsConfig;
+use stwo_cairo_prover::stwo_prover::core::vcs::blake2_merkle::{Blake2sMerkleChannel, Blake2sMerkleHasher};
 use utils::{
     bench::{benchmark, Metrics},
     metadata::{FIBONACCI_INPUTS, SHA2_INPUTS},
     sha2_input,
 };
-
-use cairo_air::verifier::verify_cairo;
-use cairo_air::PreProcessedTraceVariant;
-use cairo_lang_runner::Arg;
-use cairo_prove::execute::execute;
-use cairo_prove::prove::{prove, prover_input_from_runner};
-use cairo_vm::Felt252;
-use sonic_rs;
-use std::env;
-use stwo_cairo_prover::stwo_prover::core::fri::FriConfig;
-use stwo_cairo_prover::stwo_prover::core::pcs::PcsConfig;
-use stwo_cairo_prover::stwo_prover::core::vcs::blake2_merkle::Blake2sMerkleChannel;
 
 /// Configurations for the CSTARK prover.
 ///
@@ -32,6 +46,158 @@ pub const REGULAR_96_BITS: PcsConfig = PcsConfig {
         n_queries: 80,
     },
 };
+
+/// Executes a Cairo program and returns a `CairoRunner` that can be used to generate artifacts for
+/// the prover.
+pub fn execute(executable: Executable, args: Vec<Arg>) -> CairoRunner {
+    let (program, string_to_hint) = program_and_hints_from_executable(&executable);
+
+    let mut hint_processor = CairoHintProcessor {
+        runner: None,
+        user_args: vec![vec![Arg::Array(args)]],
+        string_to_hint,
+        starknet_state: Default::default(),
+        run_resources: Default::default(),
+        syscalls_used_resources: Default::default(),
+        no_temporary_segments: false,
+        markers: Default::default(),
+        panic_traceback: Default::default(),
+    };
+
+    let cairo_run_config = CairoRunConfig {
+        trace_enabled: true,
+        relocate_mem: true,
+        layout: LayoutName::all_cairo_stwo,
+        secure_run: None,
+        allow_missing_builtins: None,
+        dynamic_layout_params: None,
+        disable_trace_padding: true,
+        proof_mode: true,
+        ..Default::default()
+    };
+
+    info!("Executing program...");
+    let runner = cairo_run_program(&program, &cairo_run_config, &mut hint_processor)
+        .expect("Failed to execute program");
+    info!("Program executed successfully.");
+    runner
+}
+
+fn program_and_hints_from_executable(executable: &Executable) -> (Program, HashMap<String, Hint>) {
+    let data: Vec<MaybeRelocatable> = executable
+        .program
+        .bytecode
+        .iter()
+        .map(Felt252::from)
+        .map(MaybeRelocatable::from)
+        .collect();
+    let (hints, string_to_hint) = build_hints_dict(&executable.program.hints);
+    let entrypoint = executable
+        .entrypoints
+        .iter()
+        .find(|e| matches!(e.kind, EntryPointKind::Standalone))
+        .expect("Failed to find entrypoint");
+    let program = Program::new_for_proof(
+        entrypoint.builtins.clone(),
+        data,
+        entrypoint.offset,
+        entrypoint.offset + 4,
+        hints,
+        Default::default(),
+        Default::default(),
+        vec![],
+        None,
+    )
+    .unwrap();
+    (program, string_to_hint)
+}
+
+///
+/// Replacement of the functions from the `cairo-prove` crate that are conflicting with
+/// the revision of `stwo` to use.
+///
+
+// Deduces the preprocessed trace variant needed for the specific execution, and proves.
+pub fn prove(input: ProverInput, pcs_config: PcsConfig) -> CairoProof<Blake2sMerkleHasher> {
+    // Currently there are two variants of the preprocessed trace:
+    // - Canonical: Pedersen is included in the program.
+    // - CanonicalWithoutPedersen: Pedersen is not included in the program.
+    // We deduce the variant based on weather the pedersen builtin is included in the program.
+    let preprocessed_trace = match input.public_segment_context[1] {
+        true => PreProcessedTraceVariant::Canonical,
+        false => PreProcessedTraceVariant::CanonicalWithoutPedersen,
+    };
+    prove_inner(input, preprocessed_trace, pcs_config)
+}
+
+fn prove_inner(
+    input: ProverInput,
+    preprocessed_trace: PreProcessedTraceVariant,
+    pcs_config: PcsConfig,
+) -> CairoProof<Blake2sMerkleHasher> {
+    stwo_cairo_prover::prover::prove_cairo::<Blake2sMerkleChannel>(
+        input,
+        pcs_config,
+        preprocessed_trace,
+    )
+    .unwrap()
+}
+
+pub fn prover_input_from_runner(runner: &CairoRunner) -> ProverInput {
+    let public_input = runner.get_air_public_input().unwrap();
+    let addresses = public_input
+        .public_memory
+        .iter()
+        .map(|entry| entry.address as u32)
+        .collect::<Vec<_>>();
+    let segments = public_input
+        .memory_segments
+        .iter()
+        .map(|(&k, v)| {
+            (
+                k,
+                MemorySegmentAddresses {
+                    begin_addr: v.begin_addr,
+                    stop_ptr: v.stop_ptr,
+                },
+            )
+        })
+        .collect::<HashMap<_, _>>();
+    let trace = runner
+        .relocated_trace
+        .as_ref()
+        .unwrap()
+        .iter()
+        .map(|x| RelocatedTraceEntry {
+            ap: x.ap,
+            fp: x.fp,
+            pc: x.pc,
+        })
+        .collect::<Vec<_>>();
+    let mem = runner
+        .relocated_memory
+        .iter()
+        .enumerate()
+        .filter_map(|(i, x)| {
+            x.as_ref().map(|value| MemoryEntry {
+                address: i as u64,
+                value: unsafe { std::mem::transmute::<[u8; 32], [u32; 8]>(value.to_bytes_le()) },
+            })
+        });
+    let mem = MemoryBuilder::from_iter(MemoryConfig::default(), mem);
+    let main_args = runner
+        .get_program()
+        .iter_builtins()
+        .copied()
+        .collect::<Vec<_>>();
+
+    let main_args_slice: &[BuiltinName] = &main_args;
+    let public_segment_context = PublicSegmentContext::new(main_args_slice);
+
+    let input =
+        adapt_to_stwo_input(&trace, mem, addresses, &segments, public_segment_context).unwrap();
+    input
+}
 
 fn bench_cairo_fib(n: u32) -> Metrics {
     let mut metrics = Metrics::new(n as usize);
